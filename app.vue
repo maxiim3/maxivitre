027b0b741c4c3f0ead589bfb4d53a3d934b419ec
Maxiim3 <maximemickael.contact@gmail.com>
2024-11-19 13:35:29 +0100

    Add footer to UI

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <template>
   <NuxtPage />
+  <TheFooter/>
 </template>
 
 <script setup lang="ts">
@@ -12,8 +13,9 @@ useHead({
 
 <style>
 @import url("https://fonts.googleapis.com/css2?family=KoHo:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,200;1,300;1,400;1,500;1,600;1,700&family=Permanent+Marker&display=swap");
-*{
+* {
   font-family: "Koho", "Inter", sans-serif;
   scroll-behavior: smooth;
+
 }
 </style>
